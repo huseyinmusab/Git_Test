85f346033df5ae20fb2870ff9cb92f3de39cc5ea
huseyinmusab <69244411+huseyinmusab@users.noreply.github.com>
2020-08-07 15:19:20 +0200

    Update TestClassNew.java

--- src/TestClassNew.java
+++ src/TestClassNew.java
@@ -1,5 +1,6 @@
 public class TestClassNew {
     public static void main(String[]  args) {
             System.out.println("Git den merhaba");
+         System.out.println("GitHubb dan merhaba")
     }
 }
