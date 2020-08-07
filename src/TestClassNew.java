71282dbd15e3c4debb59816641495ae479e8827c
huseyin <huseyinmkocal@gmail.com>
2020-08-07 15:28:42 +0200

    .

--- src/TestClassNew.java
+++ src/TestClassNew.java
@@ -1,6 +1,7 @@
 public class TestClassNew {
     public static void main(String[]  args) {
             System.out.println("Git den merhaba");
-         System.out.println("GitHubb dan merhaba")
+         System.out.println("GitHubb dan merhaba");
+        System.out.println("Ihsan iyi bir kardesimiz");
     }
 }
